457d14f54657266d59a845dbbf0238876b83399a
luoluomo <17396243860@163.com>
2019-07-28 21:32:54 +0800

    add: 什么是 JSX 语法

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-
+// JSX语法里面， 有两类型的标签
+// 1. html标签 首字母小写
+// 2. 组件标签 首字母大写 (若小写无法渲染)
 class App extends Component {
   
   render(){
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import Test from './Test';
 
-ReactDOM.render(<div><App /><Test /></div>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
